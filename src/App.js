e320ed7f91d7c81283f4897083fd88f5ae24660f
btj43 <bethjones543@gmail.com>
2024-11-04 20:50:21 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import Search from "./Search";
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <h2> This is not working </h2>
+      <Search />
+    </div>
+  );
+}
